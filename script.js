18d61ffac0028c2ee13c388708a80b54770df575
Abdullah <abdullahbekawi135@gmail.com>
2024-12-01 20:02:24 +0300

    v3

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ canvas.height = window.innerHeight;
 const particles = [];
 const floatingParticles = [];
 const texts = [
-  { text: "BonBon", size: 106 },
+  { text: "Abdallah", size: 150 },
   { text: "Github Session", size: 80 },
 ];
 
